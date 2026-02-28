2a80113f4fd47d2d1b30b1aafbe8b290684ac761
Yar14k <tabachuk1406@gmail.com>
2026-02-28 14:39:00 +0100

    Add solution

--- src/components/TodoInfo/TodoInfo.tsx
+++ src/components/TodoInfo/TodoInfo.tsx
@@ -2,7 +2,10 @@ import { UserInfo } from '../UserInfo';
 
 export const TodoInfo = ({ todo }) => {
   return (
-    <article data-id={todo.id} className={`TodoInfo ${todo.completed ? 'TodoInfo--completed' : ''}`}>
+    <article
+      data-id={todo.id}
+      className={`TodoInfo ${todo.completed ? 'TodoInfo--completed' : ''}`}
+    >
       <h2 className="TodoInfo__title">{todo.title}</h2>
 
       <UserInfo user={todo.user} />
--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -1,9 +1,11 @@
 import { TodoInfo } from '../TodoInfo';
 
 export const TodoList = ({ todos }) => {
-  <section className="TodoList">
-    {todos.map(todo => (
-      <TodoInfo key={todo.id} todo={todo} />
-    ))}
-  </section>;
+  return (
+    <section className="TodoList">
+      {todos.map(todo => (
+        <TodoInfo key={todo.id} todo={todo} />
+      ))}
+    </section>
+  );
 };
